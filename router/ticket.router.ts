05897b4a1dea63e636fbfb95e8018712a4a4b427
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-04-29 11:48:39 -0400

    [GET:Route->my-tickets] added route to get my tickets

--- router/ticket.router.ts
+++ router/ticket.router.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import BOOK_TICKET from '../controllers/tickets/book-ticket/route';
+import GET_TICKETS from '../controllers/tickets/get-tickets/userId/route'
 import auth from "../middlewares/auth/auth.middleware";
 const ticketRouter = Router();
-ticketRouter.post('/book-ticket',auth.validateUser, BOOK_TICKET);
+ticketRouter.use(auth.validateUser);
+ticketRouter.post('/book-ticket', BOOK_TICKET);
+ticketRouter.get('/my-tickets', GET_TICKETS);
 export default ticketRouter;
