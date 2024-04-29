88a782a730142932b3040e124fafbaa85dac2ae5
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-04-29 14:13:57 -0400

    [Feature->query] query and filter feature is added in ticket and movies route

--- controllers/movies/route.ts
+++ controllers/movies/route.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
 import prisma from "../../prisma";
+import { querySchema } from "../../utils/common.schema";
 export default async function GET(req:Request, res:Response){
     try {
+        const limit = Number(req.query.limit);
+        const page = Number(req.query.page);
+        const order = req.query.order;
+        const queryValidationResult = querySchema.safeParse({limit, page, order});
+        if(!queryValidationResult.success)
+            return res.status(400).json({isError:true, result:queryValidationResult});
+        const query = queryValidationResult.data;
         const movies = await prisma.movie.findMany({
+            skip:(query.page-1)*query.limit,
+            take:limit,
+            orderBy:{
+                show_time:query.order
+            },
             select:{id:true, name:true,show_time:true, seat_available:true}
         });
         return res.status(200).json(movies);
-
     } catch (error) {
         return res.status(500).json("Internal server error");
     }
--- controllers/tickets/route.ts
+++ controllers/tickets/route.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from "express";
 import prisma from "../../prisma";
+import { querySchema } from "../../utils/common.schema";
 export default async function GET(req:Request, res:Response){
     try {
+        const limit = Number(req.query.limit);
+        const page = Number(req.query.page);
+        const order = req.query.order;
+        const queryValidationResult = querySchema.safeParse({limit, page, order});
+        if(!queryValidationResult.success)
+            return res.status(400).json({isError:true, result:queryValidationResult});
+        const query = queryValidationResult.data;
         const tickets = await prisma.ticket.findMany({
+            skip:(query.page-1)*query.limit,
+            orderBy:{
+                createdAt:query.order
+            },
             include:{movie:true}
         });
         return res.status(200).json(tickets);
