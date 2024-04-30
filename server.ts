5756d8648ceaee691bf0045d66a08c50e6727cb7
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-04-30 00:14:47 -0400

    [Feature->rate-limiting]

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express, { Response } from 'express';
+import rateLimit from 'express-rate-limit';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import authRouter from './router/auth.router';
@@ -6,11 +7,18 @@ import ticketRouter from './router/ticket.router';
 import movieRouter from './router/movie.router';
 const PORT = process.env.PORT || 5000;
 const app = express();
+const limit = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 10, // Limit each IP to 10 requests per window
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 app.use(cors());
+app.use(limit);
 app.use(express.json());
 app.use(cookieParser())
 app.use('/api/auth', authRouter);
 app.use('/api/ticket', ticketRouter);
 app.use('/api/movies', movieRouter);
-app.use('/api/helloworld', (_, res:Response)=>res.status(200).json('Hello World'));
-app.listen(PORT, ()=>console.log('Server is listening to port ', PORT));
+app.use('/api/helloworld', (_, res: Response) => res.status(200).json('Hello World'));
+app.listen(PORT, () => console.log('Server is listening to port ', PORT));
